8bd49e0b679879a06b3755c0195c9d873086cf7e
Daniel Tome Fernandez <danieltomefer@gmail.com>
2025-06-25 21:33:26 +0200

    fix: Correct import statement syntax in AppProvider component

--- components/providers/app-provider.tsx
+++ components/providers/app-provider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ReactNode } from "react";
-import { Toaster } from "@/components/ui/sonner"
+import { Toaster } from "@/components/ui/sonner";
 
 export const AppProvider = ({ children }: { children: ReactNode }) => {
   return (
