a346c6aca1da9364b2ac6c49bb89cecb525e5a69
Daniel Tome Fernandez <danieltomefer@gmail.com>
2025-06-25 12:07:21 +0200

    feat: Implement forum store with Zustand for managing communities, threads, and replies

---
+++ stores/forum-store.ts
@@ -0,0 +1,44 @@
+import { Community, Reply, Thread } from "@/types/common";
+import { create } from "zustand";
+
+interface ForumState {
+  communities: Record<string, Community>;
+  threads: Record<string, Thread>;
+  replies: Record<string, Reply>;
+  setCommunities: (communities: Community[]) => void;
+  setThreads: (threads: Thread[]) => void;
+  setReplies: (replies: Reply[]) => void;
+  addCommunity: (community: Community) => void;
+  addThread: (thread: Thread) => void;
+  addReply: (reply: Reply) => void;
+}
+
+export const useForumStore = create<ForumState>(set => ({
+  communities: {},
+  threads: {},
+  replies: {},
+  setCommunities: communities =>
+    set({
+      communities: Object.fromEntries(communities.map(c => [c.id, c])),
+    }),
+  setThreads: threads =>
+    set({
+      threads: Object.fromEntries(threads.map(t => [t.id, t])),
+    }),
+  setReplies: replies =>
+    set({
+      replies: Object.fromEntries(replies.map(r => [r.id, r])),
+    }),
+  addCommunity: community =>
+    set(state => ({
+      communities: { ...state.communities, [community.id]: community },
+    })),
+  addThread: thread =>
+    set(state => ({
+      threads: { ...state.threads, [thread.id]: thread },
+    })),
+  addReply: reply =>
+    set(state => ({
+      replies: { ...state.replies, [reply.id]: reply },
+    })),
+}));
