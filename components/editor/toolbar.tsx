76a448110f852c101221d154fa6942595ee7f130
Daniel Tome Fernandez <danieltomefer@gmail.com>
2025-07-05 14:39:58 +0200

    feat: Add dropdown menu for list formatting options in the toolbar

--- components/editor/toolbar.tsx
+++ components/editor/toolbar.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import type { EditorExtension } from "@/components/editor/extension";
 import Button from "@/components/editor/toolbar-button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { BoldIcon, CheckSquare, ItalicIcon, List, ListOrdered, UnderlineIcon } from "lucide-react";
 import type { Editor } from "prosekit/core";
 import { useEditorDerivedValue } from "prosekit/react";
@@ -53,7 +59,7 @@ export function Toolbar() {
           onClick={items.bold.command}
           tooltip="Bold Text"
         >
-          <BoldIcon className="bold h-4 w-4" />
+          <BoldIcon className="h-4 w-4" />
         </Button>
         <Button
           pressed={items.italic.isActive}
@@ -61,7 +67,7 @@ export function Toolbar() {
           onClick={items.italic.command}
           tooltip="Italic Text"
         >
-          <ItalicIcon className="bold h-4 w-4" />
+          <ItalicIcon className="h-4 w-4" />
         </Button>
         <Button
           pressed={items.underline.isActive}
@@ -71,32 +77,48 @@ export function Toolbar() {
         >
           <UnderlineIcon className="h-4 w-4" />
         </Button>
-        <Button
-          pressed={items.bullet.isActive}
-          disabled={!items.bullet.canExec}
-          onClick={items.bullet.command}
-          tooltip="Bullet List"
-        >
-          <List className="h-4 w-4" />
-        </Button>
-
-        <Button
-          pressed={items.ordered.isActive}
-          disabled={!items.ordered.canExec}
-          onClick={items.ordered.command}
-          tooltip="Numbered List"
-        >
-          <ListOrdered className="h-4 w-4" />
-        </Button>
-
-        <Button
-          pressed={items.task.isActive}
-          disabled={!items.task.canExec}
-          onClick={items.task.command}
-          tooltip="Task List"
-        >
-          <CheckSquare className="h-4 w-4" />
-        </Button>
+        {/* List dropdown button - moved here to be next to underline */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <span>
+              <Button tooltip="Lists" pressed={items.bullet.isActive || items.ordered.isActive || items.task.isActive}>
+                <List className="h-4 w-4" />
+              </Button>
+            </span>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="start">
+            <DropdownMenuItem
+              onSelect={e => {
+                e.preventDefault();
+                items.bullet.command();
+              }}
+              disabled={!items.bullet.canExec}
+              className={items.bullet.isActive ? "bg-gray-100 font-semibold text-black" : ""}
+            >
+              <List className="mr-2 h-4 w-4" /> Bullet List
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onSelect={e => {
+                e.preventDefault();
+                items.ordered.command();
+              }}
+              disabled={!items.ordered.canExec}
+              className={items.ordered.isActive ? "bg-gray-100 font-semibold text-black" : ""}
+            >
+              <ListOrdered className="mr-2 h-4 w-4" /> Numbered List
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onSelect={e => {
+                e.preventDefault();
+                items.task.command();
+              }}
+              disabled={!items.task.canExec}
+              className={items.task.isActive ? "bg-gray-100 font-semibold text-black" : ""}
+            >
+              <CheckSquare className="mr-2 h-4 w-4" /> Task List
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   );
