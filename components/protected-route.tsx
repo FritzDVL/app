288e81df204558d29c6f2a2f0cc05fb5db6d8937
Daniel Tome Fernandez <danieltomefer@gmail.com>
2025-07-07 16:52:42 +0200

    refactor: Enable LensForum to everyone

--- components/protected-route.tsx
+++ components/protected-route.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import { NFTRequiredDialog } from "@/components/nft-required-dialog";
-import { useLensReputationNFT } from "@/hooks/use-lensreputation-nft";
-
 interface ProtectedRouteProps {
   children: React.ReactNode;
 }
@@ -12,17 +9,5 @@ interface ProtectedRouteProps {
  * Can be used to wrap entire pages or specific components
  */
 export function ProtectedRoute({ children }: ProtectedRouteProps) {
-  const { hasNFT, isLoading, error } = useLensReputationNFT();
-
-  if (isLoading) {
-    return null;
-  }
-  if (error) {
-    return <div className="py-8 text-center text-red-500">Error checking NFT: {error}</div>;
-  }
-  if (!hasNFT) {
-    return <NFTRequiredDialog open={true} />;
-  }
-
   return <>{children}</>;
 }
