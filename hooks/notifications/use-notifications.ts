bf14c09bf5395eb89ccfe4ff4db3e28736368d7a
Daniel Tome Fernandez <danieltomefer@gmail.com>
2025-07-25 10:52:38 +0200

    feat: remove unused imports and filter options in useNotifications hook

--- hooks/notifications/use-notifications.ts
+++ hooks/notifications/use-notifications.ts
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
-import { APP_ADDRESS, BASE_FEED_ADDRESS } from "@/lib/constants";
+import { APP_ADDRESS } from "@/lib/constants";
 import type { Notification } from "@lens-protocol/client";
 import { fetchNotifications } from "@lens-protocol/client/actions";
-import { NotificationType, evmAddress, useSessionClient } from "@lens-protocol/react";
+import { NotificationType, useSessionClient } from "@lens-protocol/react";
 
 export function useNotifications(sessionClient: ReturnType<typeof useSessionClient>) {
   const [notifications, setNotifications] = useState<Notification[]>([]);
@@ -23,7 +23,6 @@ export function useNotifications(sessionClient: ReturnType<typeof useSessionClie
           filter: {
             notificationTypes: [NotificationType.Mentioned, NotificationType.Commented, NotificationType.Reacted],
             apps: [APP_ADDRESS],
-            feeds: [{ feed: evmAddress(BASE_FEED_ADDRESS) }],
           },
         });
         if (result.isErr()) {
